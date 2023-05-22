f8e9af2b6f541c187cbb35448c99aad17bd444e7
= <=>
2023-05-22 17:52:29 +0100

    Create mods module

---
+++ src-tauri/src/mods.rs
@@ -0,0 +1,3 @@
+pub mod assets;
+mod config;
+pub mod user;
--- src-tauri/src/protos/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod user {
-    include!(concat!(env!("OUT_DIR"), "/degiro_tracker.user.rs"));
-}
-
-pub mod assets {
-    include!(concat!(env!("OUT_DIR"), "/degiro_tracker.assets.rs"));
-}
