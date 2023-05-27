01338222f7ab525d1e06a95f55a21186af8967a0
= <=>
2023-05-27 11:11:29 +0100

    Add tauri command to read user config

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use degiro_tracker::test;
+use degiro_tracker::mods::user::User;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -10,9 +10,16 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
-    test();
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, get_user])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn get_user() -> Result<User, String> {
+    match User::read_user_from_config() {
+        Some(user) => Ok(user),
+        _ => Err(String::from("No user config found")),
+    }
 }
